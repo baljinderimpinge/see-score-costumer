01dbf8dd40de66125171b18af07848201e9fd58a
prashantmimpinge <prashantm.impinge@gmail.com>
2024-01-17 11:16:21 +0530

    newurl

--- src/lib/constant.js
+++ src/lib/constant.js
@@ -3,7 +3,7 @@ export const domin = 'dev-3hmsijzw0t7ryxrl.us.auth0.com'
 export const clientId = 'nOMexkEiBe4dRY1p6tnBVAY8WXVVlYGY'
 
 // Api base url
-export const API_BASE_URL = 'http://seescoreapi.urtestsite.com'
+export const API_BASE_URL = 'seescoreapi.urtestsite.com'
 
 // Microsoft 
 export const MICROSOFT_CLIENT_ID = 'cbaa1b09-522a-4a11-9aa3-7c04c0cc3498'
