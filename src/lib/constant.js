b61c9fb18e2d5a839191682199a9b6fab5614623
balvinder.impinge <kuldeepgill@impingeonline.com>
2024-02-08 11:09:27 +0530

    packageuninstall

--- src/lib/constant.js
+++ src/lib/constant.js
@@ -8,4 +8,6 @@ export const clientId = 'nOMexkEiBe4dRY1p6tnBVAY8WXVVlYGY'
 // Microsoft 
 export const MICROSOFT_CLIENT_ID = 'cbaa1b09-522a-4a11-9aa3-7c04c0cc3498'
 export const AUTHORITY = 'https://login.microsoftonline.com/common'
-export const REDIRECT_URL = 'https://seescore.urtestsite.com/customer-dashboard'
+// export const REDIRECT_URL = 'http://localhost:4001/customer-dashboard'
+export const REDIRECT_URL =   'https://seescore.urtestsite.com/customer-dashboard'
+
