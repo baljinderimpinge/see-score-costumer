6a27f0d828a47564574690f06d0da99fd34e64c5
balvinder.impinge <kuldeepgill@impingeonline.com>
2024-02-12 21:01:21 +0530

    chart changes

--- src/lib/constant.js
+++ src/lib/constant.js
@@ -1,14 +1,16 @@
 // Auth0
-// export const domin = 'dev-3hmsijzw0t7ryxrl.us.auth0.com'
-// export const clientId = 'nOMexkEiBe4dRY1p6tnBVAY8WXVVlYGY'
-//  export const API_BASE_URL = 'https://seescoreapi.urtestsite.com'
-// export const REDIRECT_URL =   'https://seescore.urtestsite.com/customer-dashboard'
-
 export const domin = 'dev-3hmsijzw0t7ryxrl.us.auth0.com'
-export const clientId = '2pio3igv0Oms4xOL2D98e84PVDiVX4QA'
-// // Api base url
-export const REDIRECT_URL = 'http://localhost:4001/customer-dashboard'
-export const API_BASE_URL = 'http://localhost:4000'
+export const clientId = 'nOMexkEiBe4dRY1p6tnBVAY8WXVVlYGY'
+ export const API_BASE_URL = 'https://seescoreapi.urtestsite.com'
+export const REDIRECT_URL =   'https://seescore.urtestsite.com/customer-dashboard'
 export const MICROSOFT_CLIENT_ID = 'cbaa1b09-522a-4a11-9aa3-7c04c0cc3498'
 export const AUTHORITY = 'https://login.microsoftonline.com/common'
+
+// export const domin = 'dev-3hmsijzw0t7ryxrl.us.auth0.com'
+// export const clientId = '2pio3igv0Oms4xOL2D98e84PVDiVX4QA'
+// // // Api base url
+// export const REDIRECT_URL = 'http://localhost:4001/customer-dashboard'
+// export const API_BASE_URL = 'http://localhost:4000'
+// export const MICROSOFT_CLIENT_ID = 'cbaa1b09-522a-4a11-9aa3-7c04c0cc3498'
+// export const AUTHORITY = 'https://login.microsoftonline.com/common'
 
