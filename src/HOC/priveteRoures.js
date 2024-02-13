1b3e6299a9dbd14fe9fb0cf0fdd7526b79f6548d
balvinder <balvinder.impinge@gmail.com>
2024-02-13 13:47:07 +0530

    admin issue fix

--- src/HOC/priveteRoures.js
+++ src/HOC/priveteRoures.js
@@ -1,25 +1,24 @@
-import React from 'react';
-import { Route, Navigate, Outlet } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
-import { useIsAuthenticated, useMsal } from '@azure/msal-react';
+import React from "react";
+import { Route, Navigate, Outlet } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { useIsAuthenticated, useMsal } from "@azure/msal-react";
 // import { useMsal } from "@azure/msal-react";
 
 export const AuthenticatedRoute = () => {
-  const token = localStorage.getItem("token") 
-  return token ? <Outlet /> : <Navigate to="/" />
+    const token = localStorage.getItem("token");
+    return token ? <Outlet /> : <Navigate to="/" />;
 };
 
 export const AuthenticatedAdminRoute = () => {
-  const token = localStorage.getItem("token") 
-  const role = localStorage.getItem("role")
-  return token && role=="admin" ? <Outlet /> : <Navigate to="/" />
+    const token = localStorage.getItem("token");
+    const role = localStorage.getItem("role");
+    return token && role == "Admin" ? <Outlet /> : <Navigate to="/" />;
 };
 
 export const MicrosoftAuthenticatedRoute = () => {
-  const { instance } = useMsal();
-  const currentAccount = instance.getActiveAccount();  
-  const isAuthenticated = useIsAuthenticated();
-  console.log(isAuthenticated, "isAuthenticated")
-  return isAuthenticated  ? <Outlet /> : <Navigate to="/customer-dashboard" />
+    const { instance } = useMsal();
+    const currentAccount = instance.getActiveAccount();
+    const isAuthenticated = useIsAuthenticated();
+    console.log(isAuthenticated, "isAuthenticated");
+    return isAuthenticated ? <Outlet /> : <Navigate to="/customer-dashboard" />;
 };
-
--- src/components/Button/LoginButton.js
+++ src/components/Button/LoginButton.js
@@ -9,7 +9,6 @@
 // import 'react-toastify/dist/ReactToastify.css';
 // import { ClipLoader } from "react-spinners";
 
-
 // const FullPageLoader = styled.div`
 //   position: fixed;
 //   top: 0;
@@ -40,7 +39,6 @@
 //           let payload = {
 //             token: idToken.__raw
 //           }
-
 
 //           const resuilt = await axios.post(`${API_BASE_URL}/user/login`, payload)
 //           if (user?.email === "adminseescore@gmail.com") {
@@ -92,92 +90,107 @@
 // }
 // export default LoginButton;
 
-
-
 import React, { useEffect, useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useNavigate } from 'react-router-dom';
-import images from "../../assets/images/auth-logo.png"
+import { useNavigate } from "react-router-dom";
+import images from "../../assets/images/auth-logo.png";
 import axios from "axios";
 import { API_BASE_URL } from "../../lib/constant";
-import styled from '@emotion/styled';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import styled from "@emotion/styled";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { ClipLoader } from "react-spinners";
 
-
 const FullPageLoader = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: rgba(255, 255, 255, 0.8); /* semi-transparent white background */
-  z-index: 1000;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: rgba(
+        255,
+        255,
+        255,
+        0.8
+    ); /* semi-transparent white background */
+    z-index: 1000;
 `;
 
 const LoginButton = () => {
-  const { loginWithRedirect, user, isAuthenticated, getIdTokenClaims } = useAuth0();
-  const navigate = useNavigate();
-  const [loder,setLoder]=  useState(true)
-  useEffect(() => {
-    const fetchAndLogTokens = async () => {
-      if (!isAuthenticated) {
-        console.log("User is not authenticated");
-        return;
-      }
-      debugger
-      console.log(isAuthenticated,"isAuthenticated")
-      debugger
-      try {
-        const idToken = await getIdTokenClaims();
-        if (idToken.__raw) {
-          let payload = {
-            token: idToken.__raw
-          }
-          const result = await axios.post(`${API_BASE_URL}/user/login`, payload);
-          // setLoading(false);
-          if (user?.email === "adminseescore@gmail.com") {
-            toast.success('Login successfully!', { position: toast.POSITION.TOP_RIGHT });
-            localStorage.setItem("token", idToken.__raw);
-            navigate('/admin-dashboard');
-          } else {
-            toast.success('Login successfully!', { position: toast.POSITION.TOP_RIGHT });
-            localStorage.setItem("token", idToken.__raw);
-            localStorage.setItem("companyName", result.data.data.app_metadata.bussinessName);
-            navigate('/customer-dashboard');
-          }
-        }
-      } catch (error) {
-        console.error('Error fetching tokens:', error);
-      }
-    };
+    const { loginWithRedirect, user, isAuthenticated, getIdTokenClaims } =
+        useAuth0();
+    const navigate = useNavigate();
+    const [loder, setLoder] = useState(true);
+    // useEffect(() => {
+    //     const fetchAndLogTokens = async () => {
+    //         if (!isAuthenticated) {
+    //             console.log("User is not authenticated");
+    //             return;
+    //         }
+    //         console.log(isAuthenticated, "isAuthenticated");
+    //         try {
+    //             const idToken = await getIdTokenClaims();
+    //             if (idToken.__raw) {
+    //                 let payload = {
+    //                     token: idToken.__raw,
+    //                 };
+    //                 const result = await axios.post(
+    //                     `${API_BASE_URL}/user/login`,
+    //                     payload
+    //                 );
+    //                 console.log(result, "result   result");
+    //                 // setLoading(false);
+    //                 if (result?.data?.data?.app_metadata?.role === "Admin") {
+    //                     toast.success("Login successfully!", {
+    //                         position: toast.POSITION.TOP_RIGHT,
+    //                     });
+    //                     localStorage.setItem("token", idToken.__raw);
+    //                     navigate("/admin-dashboard");
+    //                 } else {
+    //                     toast.success("Login successfully!", {
+    //                         position: toast.POSITION.TOP_RIGHT,
+    //                     });
+    //                     localStorage.setItem("token", idToken.__raw);
+    //                     localStorage.setItem(
+    //                         "companyName",
+    //                         result.data.data.app_metadata.bussinessName
+    //                     );
+    //                     navigate("/customer-dashboard");
+    //                 }
+    //             }
+    //         } catch (error) {
+    //             console.error("Error fetching tokens:", error);
+    //         }
+    //     };
 
-    fetchAndLogTokens();
-  }, [isAuthenticated, getIdTokenClaims, navigate, user]);
+    //     fetchAndLogTokens();
+    // }, [isAuthenticated, getIdTokenClaims, navigate, user]);
 
-  if (isAuthenticated) {
+    if (isAuthenticated) {
+        return (
+            <React.Fragment>
+                <FullPageLoader>
+                    <ClipLoader size={50} color={"#000"} loading={true} />
+                </FullPageLoader>
+                <ToastContainer />
+            </React.Fragment>
+        );
+    }
+
+    // Directly show Auth0 login page
+    console.log(window.location.origin, "window.location.origin");
+    loginWithRedirect({
+        redirectUri: `${window.location.origin}/fetching-customer`,
+    });
+
     return (
-      <>
-        <FullPageLoader><ClipLoader size={50} color={'#000'} loading={true} /></FullPageLoader>
-        <ToastContainer />
-      </>
+        <FullPageLoader>
+            <ClipLoader size={50} color={"#000"} loading={true} />
+        </FullPageLoader>
     );
-  }
-
-  // Directly show Auth0 login page 
-  console.log(window.location.origin,"window.location.origin")
-  loginWithRedirect({
-    redirectUri: `${window.location.origin}/fetching-customer`
-  });
-
-  return (
-    <FullPageLoader><ClipLoader size={50} color={'#000'} loading={true} /></FullPageLoader>
-        
-  );
-}
+};
 
 export default LoginButton;
